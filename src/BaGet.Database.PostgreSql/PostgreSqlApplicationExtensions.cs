4c7240fd4eef2d2b7c7afff3e32dc5cb60f70edd
Ash39 <AnthonyFrank378@gmail.com>
2021-11-03 00:16:26 -0400

    Edited PostgreSqlProject 2

--- src/BaGet.Database.PostgreSql/PostgreSqlApplicationExtensions.cs
+++ src/BaGet.Database.PostgreSql/PostgreSqlApplicationExtensions.cs
@@ -4,6 +4,7 @@ using BaGet.Database.PostgreSql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Npgsql;
 
 namespace BaGet
 {
@@ -13,7 +14,25 @@ namespace BaGet
         {
             app.Services.AddBaGetDbContextProvider<PostgreSqlContext>("PostgreSql", (provider, options) =>
             {
+                
                 var databaseOptions = provider.GetRequiredService<IOptionsSnapshot<DatabaseOptions>>();
+
+                if(Environment.GetEnvironmentVariable("DATABASE_URL") != null)
+                {
+                    var databaseUri = new Uri(Environment.GetEnvironmentVariable("DATABASE_URL"));
+                    var userInfo = databaseUri.UserInfo.Split(':');
+
+                    var builder = new NpgsqlConnectionStringBuilder
+                    {
+                        Host = databaseUri.Host,
+                        Port = databaseUri.Port,
+                        Username = userInfo[0],
+                        Password = userInfo[1],
+                        Database = databaseUri.LocalPath.TrimStart('/')
+                    };
+
+                    databaseOptions.Value.ConnectionString = builder.ToString();
+                }
                 
                 options.UseNpgsql(databaseOptions.Value.ConnectionString);
             });
